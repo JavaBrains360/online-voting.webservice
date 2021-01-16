610e367f2133e742861a50b787a2cdc778f3cfcc
Java brains <curiousminds2314@gmail.com>
2021-01-16 14:53:55 +0530

    initial

---
+++ src/test/java/com/javabrains/project/Online/voting/system/OnlineVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabrains.project.Online.voting.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
